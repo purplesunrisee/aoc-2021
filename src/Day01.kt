e88c387f00835828c599e1a5eaa1466cc2f7f77f
purplesunrisee <purplesunrisee@gmail.com>
2021-12-01 09:29:58 +0300

    Day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(data: List<Int>): Int {
+        var previous = data[0]
+        return data.fold(0) { acc: Int, el: Int ->
+            val result = if(previous < el) { acc + 1 } else { acc }
+            previous = el
+            result
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(data: List<Int>): Int {
+        return part1((0..data.size - 3).map { data.subList(it, it + 3).sum() })
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val data = testInput.map(String::toInt)
+    check(part1(data) == 7)
+    check(part2(data) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val data2 = input.map(String::toInt)
+    println(part1(data2))
+    println(part2(data2))
 }
